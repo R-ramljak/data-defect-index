302aa9943f36cad54b6eb56ec570b6900855fd2e
Marco Ramljak <45019651+R-ramljak@users.noreply.github.com>
2021-01-25 01:11:13 +0100

    First report

--- Data exploration turnover.R
+++ Data exploration turnover.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(broom)
+library(modelr)
 
 
 raw <- readRDS("Data/sample.rds")
@@ -36,8 +38,13 @@ data %>%
 # density turnover
 data %>% 
   ggplot() +
-  geom_density(aes(log10(omzet_excl_btw), group = selective.sam, color = selective.sam)) +
-  facet_wrap(~year.quarter)
+  geom_density(aes(log10(omzet_excl_btw), 
+                   color = as.factor(selective.sam))) +
+  geom_density(aes(log10(omzet_excl_btw)), color = "black") +
+  theme(legend.position="bottom") +
+  facet_wrap(~year.quarter) +
+  labs(x = "log10(turnover)", y = "density", color = "Selection") +
+  ggtitle("Density: Log10 Turnover by Year/Quarter", subtitle = "Grouped by selection, black line population")
 
 # density days
 data %>% 
@@ -45,30 +52,40 @@ data %>%
   geom_density(aes(daysSinceEOQ)) +
   facet_wrap(~year.quarter)
 
-# density turnover
+# density working people
 data %>% 
   ggplot() +
-  geom_density(aes(log10(wp))) +
-  facet_wrap(~year.quarter)
+  geom_density(aes(log10(wp), 
+                   color = as.factor(selective.sam))) +
+  geom_density(aes(log10(wp)), color = "black") +
+  theme(legend.position="bottom") +
+  facet_wrap(~year.quarter) +
+  labs(x = "log10(working people)", y = "density", color = "Selection") +
+  ggtitle("Density: Log10 Working people by Year/Quarter", subtitle = "Grouped by selection, black line population")
+
 
 # turnover total per period
 e <- data %>% 
   group_by(year.quarter) %>% 
   summarise(tot.turnover = sum(omzet_excl_btw))
 
-# correlation plot
-data %>% 
-  ggplot() +
-  geom_point(aes(x = log10(wp), y = log.turnover)) +
-  facet_wrap(~year.quarter)
+
+## Modelling wp and log10(turnover)
+quarter_model <- function(df) {
+  lm(log.turnover ~ wp, data = df)
+}
+
+by.quarter <- data %>% 
+  group_by(year.quarter) %>% 
+  nest() %>% 
+  mutate(model = map(data, quarter_model)) %>% 
+  mutate(resids = map2(data, model, add_residuals))
+
+resids <- unnest(by.quarter, resids)
   
-
-
-## Modelling
-mod1 <- lm(log.turnover ~ wp, data = data)
-summary(mod1)
-
-y.name = "Year"
-z.name = "Quarter"
-
-
+resids %>% 
+  ggplot(aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line() +
+  facet_wrap(~year.quarter) +
+  ggtitle("Normal Q-Q")
